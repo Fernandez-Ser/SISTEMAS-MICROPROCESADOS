a59c41a37bdbef6b7c8be836f808b36294a3b8a6
Fernandez Catucuago Sergio Rolando <44453788+Fernandez-Ser@users.noreply.github.com>
2019-02-05 08:15:30 -0500

    Add files via upload

---
+++ Deber-LowPower-FernandezSergio.ino
@@ -0,0 +1,28 @@
+/* 
+ *                  UNIVERSIDAD TECNICA DEL NORTE 
+ *                  FICA-CIERCOM-SISTEMAS MICROPROCESADOS
+ * Nombre: Fernanddez Sergio
+ * Fecha:05/02/2019
+ *                     Deber
+ * Tema:Sistema que permita configurar por comunicación serial o por pulsadores el modo sleep cada 25 segundos.
+ */
+#include <LowPower.h> //libreria LowPower
+int contador = 0;  // Variable de conteo
+void setup() {
+  Serial.begin(9600); //Inicializacion de CX
+  attachInterrupt(0,sistema,FALLING); //Interrupcion con metodo
+  }
+
+ void loop() {
+     //FUNCIONAMIENTO CON INTERRUPCION
+  }
+void sistema() {
+  for (; contador>=25;contador++) {  //conteo y aumento del contador
+       LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF); // levantar al sistema
+  }
+   if(contador==25){
+   Serial.println("SISTEMA DESPIERTO"); // Mensaje a imprimir
+   contador=0; // inicializacion del contador
+   }
+  delay(200);
+   }
